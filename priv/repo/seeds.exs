e4f12fc205f1d0384db7bd638b455956b4394438
SENGA <jeuxleaveus@gmail.com>
2018-11-01 23:49:32 +0900

    modify seeds data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,17 +1,26 @@
 import NanoPlanner.Repo, only: :functions
 alias NanoPlanner.PlanItem
 
+time0 = DateTime.from_unix!(0)
+
 insert! %PlanItem{
   name: "読書",
-  description: "『走れメロス』を読む"
+  description: "『走れメロス』を読む",
+  starts_at: time0,
+  ends_at: time0
 }
 
 insert! %PlanItem{
   name: "買い物",
-  description: "洗剤を買う"
+  description: "洗剤を買う",
+  starts_at: time0,
+  ends_at: time0
+
 }
 
 insert! %PlanItem{
   name: "帰省",
-  description: "新幹線の指定席を取る。\nお土産を買う。"
+  description: "新幹線の指定席を取る。\nお土産を買う。",
+  starts_at: time0,
+  ends_at: time0
 }
